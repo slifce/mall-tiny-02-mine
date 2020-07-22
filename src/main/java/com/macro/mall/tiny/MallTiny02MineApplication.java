ee46bad30252feb0298a7fa407b4edab7294977d
Liaoyangpeng <734507707@qq.com>
2020-07-22 22:05:02 +0800

    Initial commit

---
+++ src/main/java/com/macro/mall/tiny/MallTiny02MineApplication.java
@@ -0,0 +1,13 @@
+package com.macro.mall.tiny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallTiny02MineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallTiny02MineApplication.class, args);
+	}
+
+}
